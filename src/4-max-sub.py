706038a7166e051892fe52b4fb750977b1e20225
Daniil Gurgurov <dgurgurov@gmail.com>
2024-08-04 12:57:18 +0200

    fixed error in max-sub

--- src/4-max-sub.py
+++ src/4-max-sub.py
@@ -7,9 +7,10 @@ class Solution:
         currentSum = 0 # initialize currentSum to 0
 
         for num in nums:
-            currentSum += num # add current number to currentSum
             if currentSum < 0: # if currentSum is negative, reset it to 0; we don't want to add negative numbers from previous subarrays
                 currentSum = 0 
+
+            currentSum += num # add current number to currentSum
             maxSubarray = max(maxSubarray, currentSum) # update maxSubarray with the maximum of maxSubarray and currentSum
 
         return maxSubarray
